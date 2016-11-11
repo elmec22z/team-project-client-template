8a81dce2cbb14a5fed1870238182d6ad6b1a6c03
Roxasch <kztoth@umass.edu>
2016-11-11 16:15:00 -0500

    working on result page. can't get the route to work so it currently only displays the result page

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Result from './components/result';
+
+class ResultPage extends React.Component {
+	render() {
+		return (
+				<Navbar />
+		)
+	}
+}
+
+ReactDOM.render(
+	<Result />,
+	document.getElementById('app')
+);
---
+++ app/components/navbar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+  render() {
+    return(
+    <div>
+    <link href="css/about.css" rel="stylesheet" />
+    <link href="css/template.css" rel="stylesheet" />
+    <nav className="navbar navbar-fixed-top navbar-default">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">
+            <span className="glyphicon glyphicon-home"></span>
+          </a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <form className="navbar-form navbar-left" role ="search">
+            <div className="input-group">
+              <input type = "text" className="form-control" placeholder = "Search Safehouse">
+                <span className="input-group-btn">
+                  <button type="submit" className="btn btn-default">
+                    <span className="glyphicon glyphicon-search"></span>
+                  </button>
+                </span>
+              </input>
+            </div>
+          </form>
+          <div className="nav navbar-nav navbar-right">
+            <div className="btn-toolbar pull right" role="toolbar">
+              <div className="btn-group" role="group">
+                <button type="button" className="btn btn-default navbar-btn">
+                  Home
+                </button>
+              </div>
+              <div className="btn-group" role="group">
+                <button type="button" className="btn btn-default navbar-btn">
+                  My Profile
+                </button>
+              </div>
+              <div className="btn-group" role="group">
+                <button type="button" className="btn btn-default navbar-btn">
+                  About
+                </button>
+              </div>
+              <div className="btn-group" role="group">
+                <button type="button" className="btn btn-default navbar-btn">
+                  Forum
+                </button>
+              </div>
+              <div className="btn-group" role="group">
+                <button type="button" className="btn btn-default btn btn-default navbar-btn dropdown
+                -toggle" data-toggle="dropdown">
+                  <span className="caret"></span>
+                </button>
+                <ul className="dropdown-menu">
+                  <li /><a href="#">Log Out</a>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    </div>
+    )
+  }
+}
