21e8fdde9a7e1d6feb96a07ccfc11850168b9086
Meghana Arvind <meghana.arvind@gmail.com>
2016-11-13 16:16:44 -0500

    committing app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
 /* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {IndexRouter, Router, Route, browserHistory } from 'react-router'
+
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			
+
+))
