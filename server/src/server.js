e98ee5bc3ad6822cbf5a898fd2bde1137622157e
yanglalama <lama24t@mtholyoke.edu>
2016-12-03 23:15:47 -0500

    Updated port

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,8 @@
-//import {readDocument, writeDocument, addDocument, readCollection} from './database.js';
 // Imports the express Node module.
 var express = require('express');
 // Creates an Express server.
 var app = express();
 
-//import reverseString
-//var Util = require('./util');
-//var reverseString = Util.reverseString;
 
 // import readDocument
 //var database = require ('./database');
@@ -20,14 +16,9 @@ var bodyParser = require('body-parser');
 // so this translates into `client/build`!
 app.use(express.static('../client/build'));
 
-//defines what happens when it receives the 'GET /' request
-// app.get('/', function (req, res) {
-//  res.send('Hello World!');
-// });
-
 //starts the server on port 3000:
 app.listen(3000, function () {
- console.log('Example app listening on port 3000!');
+ console.log('SafeHouse listening on port 3000!');
 });
 
 app.use(bodyParser.text());
