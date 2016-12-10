78c25a272ce14d439e3a9da300c374411a452328
Moussoun <mndiaye@umass.edu>
2016-12-10 12:21:50 -0500

    altered home page

--- client/app/components/splash.js
+++ client/app/components/splash.js
@@ -1,16 +1,61 @@
 import React from 'react';
-import Navbar from './navbar';
-import ErrorBanner from './errorbanner'
 
 export default class splash extends React.Component {
   render(){
     return (
-    	<div>
-        <Navbar />
+      <div>
 
-        <div id="background">
-          <img src="img/safehouse-worldmap.jpg" width="1500" height="979"/>
+        <div id="title">
+        SafeHouse
         </div>
+
+        <div id="login">
+          <div className="row">
+            <div className="col-md-12">
+
+                <h1> LOGIN </h1>
+                <br />
+
+
+                <div id="username">
+                  <div className="input-group">
+                    <input type = "text" className= "form-control" placeholder = "Username">
+                      <span className= "input-group-btn">
+                      </span>
+                    </div>
+                  </div>
+
+                  <div id="password">
+                    <div className="input-group">
+                      <input type = "text" className= "form-control" placeholder = "Password">
+                        <span className= "input-group-btn">
+                        </span>
+                      </div>
+                    </div>
+
+                    <br />
+                    <div className="btn-group" role="group">
+                      <button type="button" className="btn btn-default navbar-btn">
+                        Login
+                      </button>
+                    </div>
+
+                    <div className="btn-group" role="group">
+                      <button type="button" className="btn btn-default navbar-btn">
+                        Forgot Password?
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            <div>
+              About us
+            </div>
+
+          <div id="background">
+            <img src="img/safehouse.jpg"/>
+          </div>
 
       </div>
     )
