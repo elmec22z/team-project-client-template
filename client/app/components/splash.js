432b685b2e0e27ab99dd734208e3ebfe18277255
yanglalama <lama24t@mtholyoke.edu>
2016-12-10 11:42:27 -0500

    updated app.js

--- client/app/components/splash.js
+++ client/app/components/splash.js
@@ -9,12 +9,7 @@ export default class splash extends React.Component {
         <Navbar />
         <div id="background">
           <img src="img/safehouse-worldmap.jpg" width="1500" height="979"/>
-<<<<<<< HEAD
-        </div> 
-=======
         </div>
-        
->>>>>>> 70c909c747f5746fe3a51f013b433ff8f0eeea7d
       </div>
     )
 	}
