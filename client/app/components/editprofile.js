6508e10987248f4f61c3da31ae3470aa1b25ac92
cdeangelis <cdeangelis@umass.edu>
2016-12-16 23:50:31 -0500

    fixed edit-profile conflicts

--- client/app/components/editprofile.js
+++ client/app/components/editprofile.js
@@ -1,26 +1,3 @@
-import React from 'react';
-import Navbar from './navbar';
-import {getProfileData} from '../server';
-<<<<<<< HEAD
-export default class editprofile extends React.Component {
-
-    constructor(props) {
-    super(props);
-    this.state = {
-      homeDesc: "",
-      familyDesc: "",
-      AreaDesc: "",
-      Accommodations: ""
-    }
-  }
-
-  componentDidMount(){
-    getProfileData(1, (feedData)=> {
-      this.setState({homeDesc: feedData.homeDesc, familyDesc: feedData.familyDesc, AreaDesc: feedData.AreaDesc, Accommodations: feedData.Accommodations});
-    });
-  }
-
-=======
 import {getUserData} from '../server';
 import {editProfileData} from '../server';
 import {browserHistory} from 'react-router';
@@ -92,13 +69,11 @@ export default class editprofile extends React.Component {
     browserHistory.push('/profile');
   }
 
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
   render(){
     return(
       <div>
         <Navbar/>
         <div className = "container">
-<<<<<<< HEAD
           <button type="button" onclick="alert('Save Changes!')">Save Changes</button>
           <div className = "col-md-16">
             <a name="Date of birth"></a>
@@ -131,49 +106,17 @@ export default class editprofile extends React.Component {
                   <div className ="describe-your-family">
                     <textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
                     </textarea>
-=======
-
-          <button className="btn btn-default update-button"
-            onClick={(e) => this.handleSave(e)}>
-            <span className="glyphicon glyphicon-pin"></span>
-            Save and Exit
-          </button>
-
-          <div className = "col-md-16">
-            <a name="Date of birth"></a>
-            <div className = "panel panel-default">
-              <div className="panel-body">
-                <ul className ="nav nav-pills">
-                  <li role="presentation" className="active">
-                    <span><strong><font size="4">Date of birth</font></strong></span>
-                  </li>
-                </ul>
-                <div className="panel-body">
-                  <div className ="date-of-birth">
-                    <input type ="dob"
-                      type='text'
-                      id="dob"
-                      defaultvalue = {this.state.dob}
-                      onChange={(e) => this.handleDobChange(e)} />
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
                   </div>
                 </div>
               </div>
             </div>
           </div>
-<<<<<<< HEAD
           <div className = "col-md-16">
             <a name="Gender"></a>
-=======
-
-          <div className = "col-md-16">
-            <a name="familySize"></a>
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
             <div className = "panel panel-default">
               <div className="panel-body">
                 <ul className ="nav nav-pills">
                   <li role="presentation" className="active">
-<<<<<<< HEAD
                     <span><strong><font size="4">Gender</font></strong></span>
                   </li>
                 </ul>
@@ -181,36 +124,17 @@ export default class editprofile extends React.Component {
                   <div className ="describe-your-family">
                     <textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
                     </textarea>
-=======
-                    <span><strong><font size="4">Family Size</font></strong></span>
-                  </li>
-                </ul>
-                <div className="panel-body">
-                  <div className ="family-size">
-                    <input type ="family-size"
-                      type='text'
-                      id="dob"
-                      defaultvalue = {this.state.familySize}
-                      onChange={(e) => this.handleFamilySizeChange(e)} />
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
                   </div>
                 </div>
               </div>
             </div>
           </div>
-<<<<<<< HEAD
           <div className = "col-md-16">
             <a name="Home address"></a>
-=======
-
-          <div className = "col-md-16">
-            <a name="Gender"></a>
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
             <div className = "panel panel-default">
               <div className="panel-body">
                 <ul className ="nav nav-pills">
                   <li role="presentation" className="active">
-<<<<<<< HEAD
                     <span><strong><font size="4">Home address</font></strong></span>
                   </li>
                 </ul>
@@ -218,63 +142,11 @@ export default class editprofile extends React.Component {
                   <div className ="describe-your-family">
                     <textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
                     </textarea>
-=======
-                    <span><strong><font size="4">Gender</font></strong></span>
-                  </li>
-                </ul>
-                <div className="panel-body">
-                  <div className ="gender">
-                    <input type ="gender"
-                      type='text'
-                      id="dob"
-                      defaultvalue = {this.state.gender}
-                      onChange={(e) => this.handleGenderChange(e)} />
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
                   </div>
                 </div>
               </div>
             </div>
           </div>
-<<<<<<< HEAD
-          <div className = "col-md-16">
-            <a name="Email"></a>
-=======
-
-          <div className = "col-md-16">
-            <a name="Home address"></a>
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
-            <div className = "panel panel-default">
-              <div className="panel-body">
-                <ul className ="nav nav-pills">
-                  <li role="presentation" className="active">
-<<<<<<< HEAD
-                    <span><strong><font size="4">Email</font></strong></span>
-                  </li>
-                </ul>
-                <div className="panel-body">
-                  <div className ="describe-your-family">
-                    <textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
-                    </textarea>
-=======
-                    <span><strong><font size="4">Home address</font></strong></span>
-                  </li>
-                </ul>
-                <div className="panel-body">
-                  <div className ="home_address">
-                    <input type ="home_address"
-                      type='text'
-                      id="home_address"
-                      defaultvalue = {this.state.address}
-                      onChange={(e) => this.handleAddressChange(e)} />
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-<<<<<<< HEAD
-=======
-
           <div className = "col-md-16">
             <a name="Email"></a>
             <div className = "panel panel-default">
@@ -285,28 +157,16 @@ export default class editprofile extends React.Component {
                   </li>
                 </ul>
                 <div className="panel-body">
-                  <div className ="email">
-                    <input type ="email"
-                      type='text'
-                      id="email"
-                      defaultvalue = {this.state.email}
-                      onChange={(e) => this.handleEmailChange(e)} />
-
-                    {/*<textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
-                    </textarea> */}
+                  <div className ="describe-your-family">
+                    <textarea styles="resize:none" rows="1" cols="120" value={this.state.familyDesc} onChange={this.state.familyDesc}>
+                    </textarea>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
         </div>
       </div>
       )
     }
-<<<<<<< HEAD
   }
-=======
-}
->>>>>>> 34f1e00292c82da92bd6474b379379997a39daaa
